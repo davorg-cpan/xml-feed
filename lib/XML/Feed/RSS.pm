0f71bfb1f23e67c0197c67adce64457f71379c0b
Simon Wistow <simon@thegestalt.org>
2008-10-23 23:32:52 +0000

    Fix typo

--- lib/XML/Feed/RSS.pm
+++ lib/XML/Feed/RSS.pm
@@ -218,7 +218,6 @@ sub content {
         } else {
             $c = $_[0];
         }
-        my $c = ref($_[0]) eq 'XML::Feed::Content' ? $_[0]->body : $_[0];
         $item->{content}{encoded} = $c;
     } else {
         my $base;
