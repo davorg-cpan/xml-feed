6c0ab45b926f0b9521e106db64a9a5a524be96fc
Dave Cross <dave@dave.org.uk>
2012-03-12 19:49:31 +0000

    Bump version for release.

--- lib/XML/Feed.pm
+++ lib/XML/Feed.pm
@@ -12,7 +12,7 @@ use Module::Pluggable search_path => "XML::Feed::Format",
                       require     => 1,
                       sub_name    => 'formatters';
 
-our $VERSION = '0.47';
+our $VERSION = '0.48';
 our $MULTIPLE_ENCLOSURES = 0;
 our @formatters;
 BEGIN {
